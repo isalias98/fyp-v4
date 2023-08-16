bb9943049fe0a0287d4f3b1dee88f7228e348582
isalias98 <isa_lias@yahoo.com>
2023-08-17 06:35:24 +0800

    test

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,15 +5,15 @@ import { useRouter } from 'next/navigation';
 
 
 const Dashboard = () => {
-    const {data:session} = useSession();
+    const { data: session } = useSession();
     const route = useRouter();
 
-    if(session?.user?.role !== "lister") {
+    if (session?.user?.role !== "lister") {
         route.push("/denied")
     }
 
-	return <div><h1>
-        Dashboard</h1>
+    return <div>
+        <h1>Dashboard</h1>
         <h1>{session?.user?.name}</h1>
         <h1>{session?.user?.role}</h1>
         <h1>{session?.user?.email}</h1>
