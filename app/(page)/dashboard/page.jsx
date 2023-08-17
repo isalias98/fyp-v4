477783db61dad3ff124fb343c11cb12035a3be66
isalias98 <isa_lias@yahoo.com>
2023-08-17 12:52:38 +0800

    useEffect for If Statement

--- app/(page)/dashboard/page.jsx
+++ app/(page)/dashboard/page.jsx
@@ -1,18 +1,20 @@
 'use client';
-
+import { useEffect } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 
 const Dashboard = () => {
-    const {data:session} = useSession();
-    const route = useRouter();
-
-    if(session?.user?.role !== "lister") {
-        route.push("/denied")
+    const { data: session } = useSession();
+    const route = useRouter()
+    useEffect(() => {
+        if (session?.user?.role !== "lister") {
+            route.push("/denied")
+        }
     }
+    )
 
-	return <div><h1>
+    return <div><h1>
         Dashboard</h1>
         <h1>{session?.user?.name}</h1>
         <h1>{session?.user?.role}</h1>
